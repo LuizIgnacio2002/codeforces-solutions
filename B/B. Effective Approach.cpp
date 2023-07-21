abbe431fb59dd15538c75e79d679461839df9078
Luiz2002 <quineche44@gmail.com>
2023-07-20 23:57:21 -0500

    Update B. Effective Approach.cpp
    uso de unordered_map

--- B/B. Effective Approach.cpp
+++ B/B. Effective Approach.cpp
@@ -1,57 +1,42 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int VasyaBusqueda(int v[], int n, int x);
-int PetyaBusqueda(int v[], int n, int x);
-int main()
-{
-	int n; cin>>n; // cantidad de elemento del array
-	int listaValores[n]; // contiene los valores
-	int Vasya=0;
-	int Petya=0;
-	//llenado de valores
-	for(int i=0; i<n; i++)
-	{
-		cin>>listaValores[i];
-	}
-	int cantConsultas; cin>>cantConsultas; 
-	for(int i=0; i<cantConsultas; i++)
-	{
-		int x; cin>>x;
-		Vasya = Vasya + VasyaBusqueda(listaValores, n, x);
-		Petya = Petya + PetyaBusqueda(listaValores, n, x);
-		
-	}
-	cout<<Vasya<<" "<<Petya;
-	return 0;
-}
-int VasyaBusqueda(int v[], int n, int x)
-{
-	int cantCompara=0;
-	bool encontrado=false;
-	for(int i=0; i<n; i++)
-	{
-		cantCompara++;
-		if(v[i]==x)
-		{
-			encontrado=true;
-		}
-		if(encontrado==true) break;
-	}
-	return cantCompara;
-}
 
-int PetyaBusqueda(int v[], int n, int x)
-{
-	int cantCompara=0;
-	bool encontrado=false;
-	for(int i=n-1; i>=0	; i--)
-	{
-		cantCompara++;
-		if(v[i]==x)
-		{
-			encontrado=true;
-		}
-		if(encontrado==true) break;
-	}
-	return cantCompara;
+int main() {
+    long long n; // Cantidad de elementos en el vector temp
+    cin >> n;
+    
+    vector<long long> temp;
+    for(int i = 0; i < n; i++)
+    {
+        long long valor;
+        cin >> valor;
+        temp.push_back(valor);
+    }
+
+    long long m; // Cantidad de elementos en el vector temp2
+    cin >> m;
+    vector<long long> temp2;
+
+    for(int i = 0; i < m; i++)
+    {
+        long long valor;
+        cin >> valor;
+        temp2.push_back(valor);
+    }
+
+    unordered_map<long long, long long> mapa;
+    for(int i = 0; i < temp.size(); i++)
+    {
+        mapa[temp[i]] = i;
+    }
+
+    long long ans1 = 0, ans2 = 0;
+    for(int i = 0; i < temp2.size(); i++)
+    {
+        ans1 += mapa[temp2[i]] + 1;
+        ans2 += temp.size() - mapa[temp2[i]];
+    }
+    cout << ans1 << " " << ans2 << endl;
+
+    return 0;
 }
